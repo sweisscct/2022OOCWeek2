cd01c1e210293a5b3db40bfa49aa76ec69eb23c5
sweissooc2022 <114678287+sweissooc2022@users.noreply.github.com>
2022-09-29 16:44:08 +0100

    Deleted extra prints

--- src/week2/Week2.java
+++ src/week2/Week2.java
@@ -16,12 +16,7 @@ public class Week2 {
     public static void main(String[] args) {
         // TODO code application logic here
         //System.out.println("Hello Git");
-        System.out.println("Hello 2nd Branch!");
-        System.out.println("Hello 2nd Branch!");
-        System.out.println("Hello 2nd Branch!");
-        System.out.println("Hello 2nd Branch!");
-        System.out.println("Hello 2nd Branch!");
-        System.out.println("Hello 2nd Branch!");
+
     }
     
 }
