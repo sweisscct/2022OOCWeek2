229c65640ebd5fbdf88e73cf2b067b2aafda6b38
sweissooc2022 <114678287+sweissooc2022@users.noreply.github.com>
2022-09-29 16:35:48 +0100

    Commented out  Git

--- src/week2/Week2.java
+++ src/week2/Week2.java
@@ -15,7 +15,7 @@ public class Week2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello Git");
+        //System.out.println("Hello Git");
         System.out.println("Hello 2nd Branch!");
         System.out.println("Hello 2nd Branch!");
         System.out.println("Hello 2nd Branch!");
