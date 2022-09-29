0416d1b396e72bbb6a2dbe51ae3d45526f70e2e5
sweissooc2022 <114678287+sweissooc2022@users.noreply.github.com>
2022-09-29 15:58:09 +0100

    2nd Hello Git
    Added another hello Git

--- src/week2/Week2.java
+++ src/week2/Week2.java
@@ -16,6 +16,7 @@ public class Week2 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello Git");
+        System.out.println("Hello Git");
     }
     
 }
